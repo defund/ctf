f663ded8ab1755af665babb466d5fb22ac4d47b3
William Wang <defund@users.noreply.github.com>
2019-04-25 00:16:25 -0400

    Update solve.py

--- angstromctf-2019/crypto/paint/solve.py
+++ angstromctf-2019/crypto/paint/solve.py
@@ -5,7 +5,9 @@ g = random.randint(0, n) | 1
 x = random.randint(0, n)
 gx = pow(g, x, n)
 
-e = 2048-2
+# order depends on the generator g
+# for this problem, it was 2**2045
+e = 2048-3
 s = 0
 for k in range(e):
 	shift = pow(2, e-1-k, n)
